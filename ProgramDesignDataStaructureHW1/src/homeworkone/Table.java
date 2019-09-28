833a30d0f6df1fbb1f4191ccd5591d670f5268f3
mphobwcodes <36450959+mphobwcodes@users.noreply.github.com>
2019-09-28 09:53:51 -0400

    Move

--- ProgramDesignDataStaructureHW1/src/homeworkone/Table.java
+++ ProgramDesignDataStaructureHW1/src/homeworkone/Table.java
@@ -1,5 +1,40 @@
 package homeworkone;
 
+/**This class implements a linked list of key/value pairs of strings**/
+
 public class Table {
 
+	//Current Node
+	private Node mark;
+	
+	
+	//first node in the list
+	private Node First;
+	
+	public boolean insert(String key, String value) {
+		
+	}
+	public String lookUp(String key) {
+		
+	}
+	
+	public boolean update(String key, String newValue) {
+		
+	}
+	public boolean markToStart() {
+		
+	}
+	public boolean advanceMark() {
+		
+	}
+	public String keyMark() {
+		
+	}
+	public String valueAtMark() {
+		
+	}
+	
+	public int displayAll() {
+		
+	}
 }
